6870c1dfb4270f483ff199fbd2eb67916528ebb6
leoemfs <116128056+leoemfs@users.noreply.github.com>
2022-10-26 16:23:55 -0300

    Create index.php

---
+++ index.php
@@ -0,0 +1,74 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Desenvolvendo meu primeiro site</title>
+        <link rel="stylesheet" href="estilo.css">
+
+    </head>
+
+    <body>
+        
+        <div id="barra-topo">JOGOS</div> <!--fim da div barra-topo-->
+
+         <div id="menu">
+             <ul>
+                 <li>PRINCIPAL</li>
+                 <li>CORRIDA</li>
+                 <li>GAMES</li>
+                 <li>CICLISMO</li>
+                 <li>FUTEBOL</li>
+                 <li>CONTATO</li>
+             </ul>
+         </div> <!-- fim da div menu-->
+        <div id="destaques">
+            <div id="destaque1"> <img src="img/1.jpg"> </div>
+              <div id="destaque2">
+                 <div id="destaA"> <img src="img/2.jfif"></div>
+                 <div id="destaB"> <img src="img/3.jfif"></div>
+             </div>
+        </div><!--fim da div destaques-->
+
+        <div id="centro">
+            <div id="fotos">
+                <h2>FOTOS</h2>
+                <hr>
+                <img id="foto1" src="img/4.jfif">
+                <img id="foto2" src="img/5.jpg">
+                <img id="foto3" src="img/6.jfif">
+
+                <ul id="lista-fotos">
+                    <li> <img src="img/7.jpg"> </li>
+                    <li> <img src="img/8.jfif"> </li>
+                    <li> <img src="img/9.jfif"> </li>
+                    <li> <img src="img/10.jfif"> </li>
+                    <li> <img src="img/11.jfif"> </li>
+                </ul>
+
+            </div> <!--fim da div fotos-->
+            <div id="lista-jogos">
+                <h2>JOGOS</h2>
+                <hr>
+                <ul>
+                    <li>FIFA 21</li>
+                    <li>EA Sports 21</li>
+                    <li>League of Legends</li>
+                    <li>Counter Strike</li>
+                    <li>Fortnite</li>
+                    <li>Overwatch</li>
+                    <li>Free Fire</li>
+                </ul>
+            </div>
+        </div> <!--fim da div centro-->
+
+        <div id="noticias">
+            <h2>Noticías</h2>
+            <hr>
+
+            <div id="noticia1"></div>
+            <div id="noticia2"></div>
+        </div>
+    </body>
+</html>
